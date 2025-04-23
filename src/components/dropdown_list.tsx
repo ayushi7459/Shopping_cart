f52e7fb952ee41824e19e8f42550c0425d23cb93
ayushi7459 <ayushii.jain7459@gmail.com>
2025-04-23 16:06:44 +0530

    prefill on save

--- src/components/dropdown_list.tsx
+++ src/components/dropdown_list.tsx
@@ -1,43 +1,67 @@
 import { useState } from 'react';
-import dropdown_list from '../dropdown.json';
+import rawDropdown from '../dropdown.json';
+// import makeAnimated from 'react-select/animated';
 import Select from 'react-select';
+
+type dropdownItems = {
+    name:string,
+    id:number,
+    category:number
+}
+
+const dropdown_list :dropdownItems[] = rawDropdown;
 
 const Dropdown_menu = () => {
 
     const [showModal, setShowModal] = useState(false);
     const [selectedValues, setSelectedValues] = useState<{ [key: number]: string[] }>({});
     const [count, setCount] = useState(1);
+    // const animatedComponents = makeAnimated();
 
-    let Dropdowns = [];
+    
+    const storedOptions = localStorage.getItem("selected_values");
+    const Options = storedOptions ? JSON.parse(storedOptions): [];
 
-    for (let i=0; i<count; i++) {
-        const filtered_list = dropdown_list.filter((item) => (item.category === i + 1));
-        // console.log(filtered_list);
-        
-        Dropdowns.push(
-            <div key={i} className="mb-3">
-                <Select 
-                    key={i}
-                    className="form-multi-select"
-                    id="ms1"
-                    isMulti
-                    data-coreui-search="global"
-                    options={filtered_list && filtered_list.map((option) => (
-                        {
-                            value: option.name,
-                            label: option.name
+    const renderDropdowns = () =>{
+        let Dropdowns = [];
+        for (let i=0; i<=count; i++) {
+            const filtered_list = dropdown_list.filter((item) => (item.category === i + 1));
+            // console.log(filtered_list);
+
+            const options = filtered_list.map(option => ({
+                value: option.name,
+                label: option.name
+              }));
+
+            const preselectValues = Options[i+1] && Options[i+1].map((val:string)=>({
+                value:val,
+                label:val
+            }))
+            console.log("preselectValues:",preselectValues);
+
+            Dropdowns.push(
+                <div key={i} className="mb-3">
+                    <Select
+                        className="form-multi-select"
+                        id="ms1"
+                        isMulti
+                        // isLoading
+                        // components={animatedComponents}
+                        options={options}
+                        defaultValue={preselectValues}
+                        onChange={(selectedOptions) =>
+                            setSelectedValues((ele) => ({
+                                ...ele,
+                                [i + 1]: selectedOptions ? selectedOptions.map((option) => option.value) : [],
+                            }))
                         }
-                    ))}
-                    onChange={(selectedOptions) =>
-                        setSelectedValues((ele) => ({
-                            ...ele,
-                            [i + 1]: selectedOptions ? selectedOptions.map((option) => option.value) : [],
-                        }))
-                    }
-                />
-            </div>
-        )
+                    />
+                </div>
+            )
+        }
+        return Dropdowns;
     }
+
     // console.log(selectedValues);
 
     const handleIncrease = () => {
@@ -48,7 +72,7 @@ const Dropdown_menu = () => {
         if (count < 1) {
             setShowModal(false);
         }
-    }
+    } 
 
     return (
         <div className='dropdown_page'>
@@ -58,29 +82,15 @@ const Dropdown_menu = () => {
                 </button>
 
                 {showModal &&
-                    <div className='modal d-block'
-                        style={{
-                            backgroundColor: 'rgba(0,0,0,0.5)',
-                            position: 'fixed',
-                            top: 0,
-                            left: 0,
-                            width: '100%',
-                            height: '100%',
-                        }}
-                    >
-                        <div className='modal-dialog mt-5'>
+                    <div className='modal d-block overlay'>
+                        <div className='modal-dialog modal-dialog-centered'>
                             <div className="modal-content border border-info border-5 rounded-4">
                                 <div className="modal-header">
-                                    <h5 className="modal-title">Multi Select Dropdowns</h5>
-                                    <button className="btn btn-secondary mx-2" onClick={handleIncrease}>
-                                        +
-                                    </button>
-                                    <button className="btn btn-secondary mx-2" onClick={handleDecrease}>
-                                        -
-                                    </button>
+                                    <button className="btn btn-secondary mx-2" onClick={handleIncrease}> + </button>
+                                    <button className="btn btn-secondary mx-2" onClick={handleDecrease}> - </button>
                                     <button className="btn-close" onClick={() => setShowModal(false)} />
                                 </div>
-                                <div className="modal-body">{Dropdowns}</div>
+                                <div className="modal-body">{renderDropdowns()}</div>
                                 <div className="modal-footer">
                                     <button className="btn btn-secondary" onClick={() => setShowModal(false)}>
                                         Close
@@ -90,10 +100,8 @@ const Dropdown_menu = () => {
                                         onClick={() => {
                                             console.log('Selected Values:', selectedValues);
                                             setShowModal(false);
-                                        }}
-                                    >
-                                        Save changes
-                                    </button>
+                                            localStorage.setItem("selected_values" ,JSON.stringify(selectedValues));
+                                        }}> Save changes </button>
                                 </div>
                             </div>
                         </div>
